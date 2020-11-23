872cc7919f02ad5a958b1d3c514a986bcb0dbff2
kietnguyentuan69 <kietnguyentuan69@gmail.com>
2020-11-23 14:03:06 +0700

    - Refactor code, handle type Time

--- excel.go
+++ excel.go
@@ -9,31 +9,6 @@ import (
 	"time"
 )
 
-func ExportMaps(ctx context.Context, headers []string, arr []map[interface{}]interface{}, format string) {
-	f := NewFile()
-	for i, header := range headers {
-		headerCellName, _ := CoordinatesToCellName(i+1, 1)
-		f.SetCellValue("Sheet1", headerCellName, header)
-		for index, rowMap := range arr {
-			cellName, _ := CoordinatesToCellName(i+1, index+2)
-			if v, ok := rowMap[header].(time.Time); ok {
-				time, err := timeToExcelTime(v.UTC())
-				if err != nil {
-					fmt.Println(err)
-				}
-				f.SetCellValue("Sheet1", cellName, time)
-				style, _ := f.NewStyle(&Style{NumFmt: 22})
-				f.SetCellStyle("Sheet1", cellName, cellName, style)
-			} else {
-				f.SetCellValue("Sheet1", cellName, rowMap[header])
-			}
-		}
-	}
-	if err := f.SaveAs(fmt.Sprintf("export_%v.%v ", strconv.Itoa(int(time.Now().Unix())), format)); err != nil {
-		println(err.Error())
-	}
-}
-
 const (
 	dayNanoseconds = 24 * time.Hour
 	maxDuration    = 290 * 364 * dayNanoseconds
@@ -43,6 +18,47 @@ var (
 	excelMinTime1900      = time.Date(1899, time.December, 31, 0, 0, 0, 0, time.UTC)
 	excelBuggyPeriodStart = time.Date(1900, time.March, 1, 0, 0, 0, 0, time.UTC).Add(-time.Nanosecond)
 )
+
+func ExportMaps(ctx context.Context, headers []string, arr interface{}, format string) {
+	if rows, ok := arr.([]interface{}); ok {
+		f := NewFile()
+		for i, header := range headers {
+			headerCellName, _ := CoordinatesToCellName(i+1, 1)
+			f.SetCellValue("Sheet1", headerCellName, header)
+			for index, rowMap := range rows {
+				if row, ok2 := rowMap.(map[string]interface{}); ok2 {
+					cellName, _ := CoordinatesToCellName(i+1, index+2)
+					switch value := row[header].(type) {
+					case string:
+						timeValue, err := time.Parse(time.RFC3339, value)
+						if err == nil {
+							FormatStringToTime(timeValue, f, cellName)
+						} else {
+							f.SetCellValue("Sheet1", cellName, row[header])
+						}
+						break
+					case time.Time:
+						FormatStringToTime(value, f, cellName)
+						break
+					default:
+						f.SetCellValue("Sheet1", cellName, row[header])
+					}
+				}
+
+			}
+		}
+		if err := f.SaveAs(fmt.Sprintf("export_%v.%v ", strconv.Itoa(int(time.Now().Unix())), format)); err != nil {
+			println(err.Error())
+		}
+	}
+}
+
+func FormatStringToTime(t time.Time, f *File, cellName string) {
+	time, _ := timeToExcelTime(t.UTC())
+	f.SetCellValue("Sheet1", cellName, time)
+	style, _ := f.NewStyle(&Style{NumFmt: 22})
+	f.SetCellStyle("Sheet1", cellName, cellName, style)
+}
 
 func timeToExcelTime(t time.Time) (float64, error) {
 	if t.Location() != time.UTC {
